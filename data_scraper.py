5d315ebd1fa693e2d551bbcfa9cf4e4c15e60463
Dennis Deng <dennisdeng2002@yahoo.com>
2016-02-08 01:25:35 -0500

    Average Positional Height Comparison

--- data_scraper.py
+++ data_scraper.py
@@ -36,17 +36,26 @@ def get_raw_player_data():
 
         for names in active_players_names:
             name_data = names.children.__next__()
-            position_data = name_data.findNext('td').findNext('td').findNext('td')
-            height_data = position_data.findNext('td')
-            weight_data = height_data.findNext('td')
+            try:
+                height_data = name_data.findNext('td').findNext('td').findNext('td').findNext('td')
+                weight_data = height_data.findNext('td')
+                url = 'http://www.basketball-reference.com/' + name_data.attrs['href'];
 
-            player_data.append((name_data.contents[0],
-                            #url to player profile page
-                            'http://www.basketball-reference.com/' + name_data.attrs['href'],
-                            position_data.contents[0],
-                            height_data.contents[0],
-                            weight_data.contents[0]
-                            ))
+                player_page = get_soup(url)
+                #subtracting the last 3 characters removes the extra dot that accompanies the position
+                position = player_page.find(text='Position:').parent.next_sibling[1:-3]
+                shooting_hand = player_page.find(text='Shoots:').parent.next_sibling[1:]
+
+                player_data.append((
+                    name_data.contents[0],
+                    position,
+                    convert_height_to_inches(height_data.contents[0]),
+                    weight_data.contents[0],
+                    shooting_hand
+                ))
+            except:
+                #Exceptions usually arise when data is missing from BballRef (results in Nonetype)
+                pass
 
     return player_data
 
@@ -55,16 +64,35 @@ def save_raw_player_data():
     raw_player_data = numpy.array(get_raw_player_data())
     numpy.save('raw_player_data.npy', raw_player_data)
 
+def convert_height_to_inches(height):
+    height_split = height.split("-")
+    feet = int(height_split[0]) * 12
+    return feet + int(height_split[1])
 
-def get_basic_comparison(height, num):
+def get_statistics_for_height(position):
+    raw_player_data = numpy.load('raw_player_data.npy')
+    heights = []
+    for data in raw_player_data:
+        if position in data[1]:
+            heights.append(int(data[2]))
+
+    heights = numpy.array(heights)
+    return [numpy.average(heights), numpy.std(heights)]
+
+def get_basic_comparison_for_height(my_height, my_position, num_of_players):
     names = list()
+    # arbitrarily define average PG height at local gym as 5'8
+    local_height = 68
+    height_data = get_statistics_for_height(my_position)
+    converted_height = height_data[0] + (my_height - local_height)
     # path must be specified for this to work in Flask
     raw_player_data = numpy.load('/Users/dennisdeng2002/Documents/Programming/PycharmProjects/nba-comparison/raw_player_data.npy')
+
     for data in raw_player_data:
-        if data[3] == height:
-            names.append([data[0], 1])
-        else:
-            names.append([data[0], 0])
+        points = 100 - numpy.abs(converted_height - int(data[2]))/height_data[1]
+        names.append((data[0], points))
 
     names.sort(key=itemgetter(1), reverse=True)
-    return names[0:num]
+    return names[0:num_of_players]
+
+print(get_basic_comparison_for_height(78, "Point Guard", 10))
